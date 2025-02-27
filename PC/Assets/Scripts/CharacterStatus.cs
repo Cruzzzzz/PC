49dbbc20bc8a555ca2b31f51d585afd40cf27a23
Arthur da Cruz <cruzz16.mr@gmail.com>
2025-02-27 16:38:54 -0300

    add assets new

--- PC/Assets/Scripts/CharacterStatus.cs
+++ PC/Assets/Scripts/CharacterStatus.cs
@@ -4,31 +4,28 @@ using UnityEngine;
 
 public abstract class CharacterStatus : MonoBehaviour
 {
-    [SerializeField] float LifeMax;
-    [SerializeField] float Speed;
-    float Life;
+    [SerializeField] float lifeMax;
+    [SerializeField] float speed;
+    float life;
 
-    public float LifeMax1 { get => LifeMax; }
-    public float Speed1 { get => Speed; }
-
+    public float LifeMax { get => lifeMax;}
+    public float Speed { get => speed;}
 
     // Start is called before the first frame update
     void Awake()
     {
-        Life = LifeMax;
-
+        life = lifeMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-    protected abstract void Teste();//obriga o filho a implementar
+
+    protected abstract void Teste();//Obriga o filho a implementar
     protected virtual void Teste2()//Permite que o filho sobrescreva
     {
-
+        //Possui corpo executável
     }
-    
-    
 }
--- PC/Assets/Scripts/PlayerStatus.cs
+++ PC/Assets/Scripts/PlayerStatus.cs
@@ -2,28 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerStatus : CharacterStatus
 {
-    Rigidbody2D rb;
-    float horizontal, vertical;
-
     protected override void Teste()
     {
         
     }
-
+    protected override void Teste2()
+    {
+        //base.Teste2();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("vertical");
+        
     }
 }
